640c663584767088b8c5d9919671092d7d16849e
dkulkov <dkulkov@neoflex.ru>
2025-09-16 20:10:39 +0300

    Причесанный код по Баранцеву

--- src/test/java/ru/stqa/pft/addressbook/TestClass.java
+++ src/test/java/ru/stqa/pft/addressbook/TestClass.java
@@ -10,45 +10,74 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.fail;
+
 @Slf4j
 public class TestClass {
     private WebDriver driver;
     private boolean acceptNextAlert = true;
     private String baseUrl;
     private final StringBuffer verificationErrors = new StringBuffer();
-      @BeforeEach
+
+    @BeforeEach
     public void setUp() throws Exception {
-                driver = new ChromeDriver();
-               baseUrl = "https://www.katalon.com/";
+        driver = new ChromeDriver();
+        baseUrl = "https://www.katalon.com/";
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
-          driver.get("http://localhost/addressbook/index.php");
-          driver.findElement(By.name("user")).clear();
-          driver.findElement(By.name("user")).sendKeys("admin");
-          driver.findElement(By.id("LoginForm")).click();
-          driver.findElement(By.name("pass")).click();
-          driver.findElement(By.name("pass")).clear();
-          driver.findElement(By.name("pass")).sendKeys("secret");
-          driver.findElement(By.xpath("//input[@value='Login']")).click();
-            }
+        driver.get("http://localhost/addressbook/index.php");
+        driver.get("http://localhost/addressbook/group.php");
+        login("admin", "secret");
+    }
+
+    private void login(String userName, String password) {
+        driver.findElement(By.name("user")).clear();
+        driver.findElement(By.name("user")).sendKeys(userName);
+        driver.findElement(By.id("LoginForm")).click();
+        driver.findElement(By.name("pass")).click();
+        driver.findElement(By.name("pass")).clear();
+        driver.findElement(By.name("pass")).sendKeys(password);
+        driver.findElement(By.xpath("//input[@value='Login']")).click();
+    }
 
     @Test
     public void test1() {
+        goToGroupPage();
+        initGroupCreation();
+        fillGroupForm(new GroupData("moscow", "saratov", "chelyabinsk"));
+        submit();
+        returnGroupCreation();
+        logOut();
+    }
 
-        driver.findElement(By.linkText("groups")).click();
-        driver.get("http://localhost/addressbook/group.php");
-        driver.findElement(By.name("new")).click();
+    private void logOut() {
+        driver.findElement(By.linkText("Logout")).click();
+    }
+
+    private void returnGroupCreation() {
+        driver.findElement(By.linkText("group page")).click();
+    }
+
+    private void submit() {
+        driver.findElement(By.name("submit")).click();
+    }
+
+    private void fillGroupForm(GroupData groupData) {
         driver.findElement(By.name("group_name")).click();
         driver.findElement(By.name("group_name")).clear();
-        driver.findElement(By.name("group_name")).sendKeys("555");
+        driver.findElement(By.name("group_name")).sendKeys(groupData.name());
         driver.findElement(By.name("group_header")).click();
         driver.findElement(By.name("group_header")).clear();
-        driver.findElement(By.name("group_header")).sendKeys("666");
+        driver.findElement(By.name("group_header")).sendKeys(groupData.header());
         driver.findElement(By.name("group_footer")).click();
         driver.findElement(By.name("group_footer")).clear();
-        driver.findElement(By.name("group_footer")).sendKeys("777");
-        driver.findElement(By.name("submit")).click();
-        driver.findElement(By.linkText("group page")).click();
-        driver.findElement(By.linkText("Logout")).click();
+        driver.findElement(By.name("group_footer")).sendKeys(groupData.footer());
+    }
+
+    private void initGroupCreation() {
+        driver.findElement(By.name("new")).click();
+    }
+
+    private void goToGroupPage() {
+        driver.findElement(By.linkText("groups")).click();
     }
 
     @AfterEach

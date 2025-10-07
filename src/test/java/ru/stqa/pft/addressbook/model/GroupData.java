2ec6d5ef26c8e1b279e4e22a96541a975f8eb5f2
dkulkov <dkulkov@neoflex.ru>
2025-10-07 16:39:49 +0300

    сравнение списков для удаления групп

--- src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,4 +1,25 @@
 package ru.stqa.pft.addressbook.model;
 
+import java.util.Objects;
+
 public record GroupData(String name, String header, String footer) {
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        GroupData groupData = (GroupData) o;
+        return Objects.equals(name, groupData.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
 }
+

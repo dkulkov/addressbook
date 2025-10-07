01c5d30ac7da4f7b122f56aad66163f9b590eb24
dkulkov <dkulkov@neoflex.ru>
2025-10-07 17:53:40 +0300

    сделал множество по группам (удаление, создание, модификация)

--- src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 
@@ -15,8 +16,20 @@ public class GroupCreationTest extends TestBase {
     public void testGroupCreation() {
         app.getNavigationHelper().goToGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().createGroup(new GroupData("moscow", null, null));
+        GroupData group = new GroupData("test1", null, null);
+        app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
+
+
+        int max = 0;
+        for (GroupData g : after ) {
+            if (g.getId() > max) {
+                max = g.getId();
+            }
+        }
+        group.setId(max);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
  }
